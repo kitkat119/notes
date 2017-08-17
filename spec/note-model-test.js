870c1a554b2c8c00dbd7f4e5e561680339325010
kitkat119 <gprydal@yahoo.co.uk>
2017-08-17 11:14:01 +0100

    tests for note id property

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,6 +1,15 @@
-describe("Text property", function() {
+describe("Note model", function() {
+  var note = new Note("Hello there");
+  var noteMock = {
+    getId: function() {
+      return 0;
+  }
+  };
+
   it("returns the text of a note" , function() {
-    var note = new Note("Hello there");
     assert.isTrue(note.getText() === "Hello there");
+  })
+  it("returns a unique id" , function() {
+    assert.isTrue(noteMock.getId() === 0);
   })
 });
--- src/note-model.js
+++ src/note-model.js
@@ -1,11 +1,15 @@
 (function(exports) {
-  function Note(string) {
+  function Note(string, id) {
     this.text = string;
+    this.id = id;
   }
 
   Note.prototype.getText = function() {
     return this.text;
-
   };
+
+  Note.prototype.getId = function() {
+    return this.id;
+  }
   exports.Note = Note;
 })(this);
